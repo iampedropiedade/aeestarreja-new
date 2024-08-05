4b7ee14a5c2ef07ae7c424a84f0b073011ede5a0
Pedro Piedade <ppiedade@rawnet.com>
2024-08-05 13:38:40 +0100

    Fixes

--- service/application/themes/aee/assets/webpack.mix.js
+++ service/application/themes/aee/assets/webpack.mix.js
@@ -6,11 +6,6 @@ mix.webpackConfig({
         bootstrap: true,
     },
 });
-mix.options({
-    fileLoaderDirs:  {
-        fonts: '../app/fonts'
-    }
-});
 
 mix.define({
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
@@ -18,6 +13,6 @@ mix.define({
 
 mix.setPublicPath('../app');
 mix.copyDirectory('images', '../app/images');
-// mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', '../app/fonts')
+mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', '../app/fonts')
 mix.sass('stylesheets/main.scss', '../app/stylesheets/main.css')
 mix.js('javascript/main.js', '../app/javascript/main.js').vue()
